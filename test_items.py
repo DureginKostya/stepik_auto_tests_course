c552ec9ae64272795723d86776dda9f24ef34895
DureginKostya <kot-502@mail.ru>
2023-10-24 14:57:23 +0500

    Changed name method

--- test_items.py
+++ test_items.py
@@ -4,9 +4,9 @@ from selenium.webdriver.common.by import By
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_get_button_basket(browser):
+def test_button_add_basket(browser):
     browser.get(link)
 
-    time.sleep(5)
+    time.sleep(30)
 
     assert browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
